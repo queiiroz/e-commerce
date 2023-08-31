b68a7412afd82921b203e9d8b51f10da59a45c9b
Gleidson Queiroz <gldsnqueiroz2@gmail.com>
2023-08-31 19:29:05 -0300

    deploying roles

---
+++ src/cardProduto.js
@@ -0,0 +1,27 @@
+import { addCarrinho } from "./menuCarrinho";
+import { catalogo } from "./utilidades";
+
+export function renderCatalogo() {
+  for (const produtoCatalogo of catalogo) {
+    const cardProduct = `<div class=" shadow-lg shadow-stone-500 rounded-lg flex flex-col  w-60 h-full p-2 m-2 justify-between item-center group" 
+    id="card-1 ">
+<img 
+  src="./assets/img/${produtoCatalogo.imagem}"
+  alt="${produtoCatalogo.alt}"
+  class="group-hover:scale-110 duration-300 mx-2"
+/>
+<p class="marca text-stone-500">${produtoCatalogo.marca}</p>
+<p class="font-bold">${produtoCatalogo.nome}</p>
+<p class="text-green-900">R$${produtoCatalogo.preco}</p>
+<button id="adicionar-${produtoCatalogo.id}" class="bg-stone-900 hover:bg-stone-950 text-stone-200 p-1"><i class="fa-solid fa-cart-plus"></i></button>
+</div>`;
+
+    document.getElementById("container-produto").innerHTML += cardProduct;
+  }
+
+  for (const produtoCatalogo of catalogo) {
+    document
+      .getElementById(`adicionar-${produtoCatalogo.id}`)
+      .addEventListener("click", () => addCarrinho(produtoCatalogo.id));
+  }
+}
--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,9 +1,35 @@
-function abrirCarrinho() {
-  document.getElementById("carrinho").classList.add("right-[0]");
-  document.getElementById("carrinho").classList.remove("right-[-20rem]");
-}
+import { catalogo } from "./utilidades";
 
 function fecharCarrinho() {
+  document.getElementById("carrinho").classList.add("right-[0]");
+  document.getElementById("carrinho").classList.remove("right-[-24rem]");
+}
+
+function abrirCarrinho() {
   document.getElementById("carrinho").classList.remove("right-[0]");
-  document.getElementById("carrinho").classList.add("right-[-20rem]");
+  document.getElementById("carrinho").classList.add("right-[-24rem]");
+}
+
+export function inicializarCarrinho() {
+  const botaoFecharCarrinho = document.getElementById("fechar-carrinho");
+  const botaoAbrirCarrinho = document.getElementById("abrir-carrinho");
+
+  botaoAbrirCarrinho.addEventListener("click", fecharCarrinho);
+  botaoFecharCarrinho.addEventListener("click", abrirCarrinho);
+}
+
+export function addCarrinho(idProduto) {
+  const produto = catalogo.find((p) => p.id === idProduto);
+  const containerProdutosCarrinho =
+    document.getElementById("produtos-carrinho");
+  const cardCarrinho = `<article class="flex bg-stone-400 text-stone-900 rounded-lg relative">
+  <button class="absolute top-1 right-2"><i class="fa-solid fa-xmark hover:text-black"></i></button>
+  <img class="h-24" src="./assets/img/${produto.imagem}" 
+  alt="${produto.alt}">
+  <div class="flex flex-col justify-center p-1">
+    <p class="text-xs">${produto.nome}</p>
+    <p class="text-green-900 font-bold">${produto.preco}</p>
+  </div>
+</article>`;
+  containerProdutosCarrinho.innerHTML += cardCarrinho;
 }
