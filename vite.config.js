a6c4e0920673f9339be2751d0a9fd1f4ee0d1522
Gleidson Queiroz <gldsnqueiroz2@gmail.com>
2023-09-06 20:06:54 -0300

    finish

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/e-commerce",
+  base: "/e-commerce/",
   plugins: [],
   resolve: {
     /*something*/
